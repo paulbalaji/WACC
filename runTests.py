38bcc08411cb57ccb57c5addae4cdf9c9ec9b132
Jan Matas <janmatas@MacBook-Pro.local>
2015-10-28 14:49:52 +0000

    [JM] Multithreaded tests

--- runTests.py
+++ runTests.py
@@ -1,11 +1,17 @@
-import glob
 import os
-files = glob.glob('valid/*.wacc')
-for f in files:
-	#print "Executing >>>>>>>> " + f
-	os.system("node parser.js " + f)
+from glob import glob
+from multiprocessing import Pool
+n_threads = 4
 
-files = glob.glob('invalid/*.wacc')
-for f in files:
-	#print "Executing >>>>>>>> " + f
-	os.system("node parser.js " + f + " errorFlag")
+def runValid(fname):
+    os.system("node parser.js " + fname + " success")
+def runInvalid(fname): 
+	os.system("node parser.js " + fname + " error")
+
+if __name__ == '__main__':
+	valid_files = [y for x in os.walk('valid') for y in glob(os.path.join(x[0], '*.wacc'))]
+	invalid_files = [y for x in os.walk('invalid') for y in glob(os.path.join(x[0], '*.wacc'))]
+
+    p = Pool(n_threads)
+    p.map(runValid, valid_files)
+    p.map(runInvalid, invalid_files)
