148e5c1a99eb2be3c43f0a3e8aedc7cd4f3f8991
Sam Wood <sam@sam.uk.net>
2015-11-09 18:16:16 +0000

    [SW][PB] Refactoring Operator Info

--- OperatorInfo.ts
+++ OperatorInfo.ts
@@ -11,12 +11,8 @@ var isType = SemanticUtil.isType;
 	Contains the possibleTypes that the operator works on and the return type of the operator
 */
 function OperatorInfo(types, returnType) {
-    this.isPermittedType = _.partial(isType, _, types);
+    this.isPermittedType = types instanceof Function ? types : _.partial(isType, _, types);
     this.returnType = returnType;
-}
-
-function genIsPermittedType(...types) {
-	return _.partial(isType, _, types[0] instanceof Array ? types[0] : types);
 }
 
 /*
@@ -26,9 +22,9 @@ var _unOpMap = {};
 
 _unOpMap['-'] = new OperatorInfo(NodeType.INT_TYPE, NodeType.INT_TYPE);
 _unOpMap['!'] = new OperatorInfo(NodeType.BOOL_TYPE, NodeType.BOOL_TYPE);
-_unOpMap['ord'] = new OperatorInfo(genIsPermittedType(NodeType.CHAR_TYPE), NodeType.INT_TYPE);
-_unOpMap['chr'] = new OperatorInfo(genIsPermittedType(NodeType.INT_TYPE), NodeType.CHAR_TYPE);
-_unOpMap['len'] = new OperatorInfo((t) => isType(t, NodeType.STRING_TYPE) || t instanceof NodeType.ArrayTypeNode, NodeType.INT_TYPE);
+_unOpMap['ord'] = new OperatorInfo(NodeType.CHAR_TYPE, NodeType.INT_TYPE);
+_unOpMap['chr'] = new OperatorInfo(NodeType.INT_TYPE, NodeType.CHAR_TYPE);
+_unOpMap['len'] = new OperatorInfo((t) => SemanticUtil.isType(t, NodeType.STRING_TYPE) || t instanceof NodeType.ArrayTypeNode, NodeType.INT_TYPE);
 
 export var unOpMap = _unOpMap;
 
@@ -43,15 +39,15 @@ _binOpMap['*']  = new OperatorInfo((t) => SemanticUtil.isType(t, NodeType.INT_TY
 _binOpMap['/']  = new OperatorInfo((t) => SemanticUtil.isType(t, NodeType.INT_TYPE), NodeType.INT_TYPE);
 _binOpMap['%']  = new OperatorInfo((t) => SemanticUtil.isType(t, NodeType.INT_TYPE), NodeType.INT_TYPE);
 _binOpMap['>']  = new OperatorInfo((t) => SemanticUtil.isType(t, [NodeType.INT_TYPE, NodeType.CHAR_TYPE]), NodeType.BOOL_TYPE);
-_binOpMap['>='] = new OperatorInfo((t) => SemanticUtil.isType(t, [NodeType.INT_TYPE, NodeType.CHAR_TYPE]), NodeType.BOOL_TYPE);
-_binOpMap['<']  = new OperatorInfo((t) => SemanticUtil.isType(t, [NodeType.INT_TYPE, NodeType.CHAR_TYPE]), NodeType.BOOL_TYPE);
-_binOpMap['<='] = new OperatorInfo((t) => SemanticUtil.isType(t, [NodeType.INT_TYPE, NodeType.CHAR_TYPE]), NodeType.BOOL_TYPE);
+_binOpMap['>='] = new OperatorInfo([NodeType.INT_TYPE, NodeType.CHAR_TYPE], NodeType.BOOL_TYPE);
+_binOpMap['<']  = new OperatorInfo([NodeType.INT_TYPE, NodeType.CHAR_TYPE]), NodeType.BOOL_TYPE);
+_binOpMap['<='] = new OperatorInfo([NodeType.INT_TYPE, NodeType.CHAR_TYPE]), NodeType.BOOL_TYPE);
 
 _binOpMap['=='] = new OperatorInfo((t) => true, NodeType.BOOL_TYPE);
 _binOpMap['!='] = new OperatorInfo((t) => true, NodeType.BOOL_TYPE);
 
-_binOpMap['&&'] = new OperatorInfo((t) => SemanticUtil.isType(t, NodeType.BOOL_TYPE), NodeType.BOOL_TYPE);
-_binOpMap['||'] = new OperatorInfo((t) => SemanticUtil.isType(t, NodeType.BOOL_TYPE), NodeType.BOOL_TYPE);
+_binOpMap['&&'] = new OperatorInfo(NodeType.BOOL_TYPE, NodeType.BOOL_TYPE);
+_binOpMap['||'] = new OperatorInfo(NodeType.BOOL_TYPE, NodeType.BOOL_TYPE);
 
 export var binOpMap = _binOpMap;
 
