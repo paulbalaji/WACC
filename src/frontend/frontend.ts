bddddd2611d9528e7f345d18156d2e4158b48f9e
Sam Wood <sam@sam.uk.net>
2015-11-16 16:42:20 +0000

    [SW] Formatting of grammar and nodetype
    Signed-off-by: Sam Wood <sam@sam.uk.net>

--- src/frontend/frontend.ts
+++ src/frontend/frontend.ts
@@ -1,4 +1,3 @@
-///<reference path="Constants.ts"/>
 var parser = require('./grammar/grammar');
 var _ = require('underscore');
 
@@ -10,23 +9,22 @@ import WACCError = require('./WACCError');
 import fs = require('fs');
 
 export function parse(programStr) {
-  
-            // Parse the input and create ast.
-            try {
-                var ast: NodeType.Visitable = parser.parse(programStr);
-            } catch (e) { 
-                throw new WACCError.SyntaxError(e);
-            }
-            
-            return ast;
-            // If the file was not supposed to throw an error, write the filename.
-            // For testing purposes only.
+    // Parse the input and create ast.
+    try {
+        var ast: NodeType.Visitable = parser.parse(programStr);
+    } catch (e) { 
+        throw new WACCError.SyntaxError(e);
+    }
+    
+    return ast;
 };
 
-
 export function semanticCheck(ast) {
-    // Execute semantic check on the input (throws error in 
-            // case of failure).
+    /* 
+        Execute semantic check on the input (throws error in 
+        case of failure).
+    */
+    
     var semanticVisitor = new SemanticChecker.SemanticVisitor();
     ast.visit(semanticVisitor);
-}
+};
