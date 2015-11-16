623338bded7002009d77401067c35f201a5d59e0
aj3214 <aj3214@ic.ac.uk>
2015-11-16 20:00:35 +0000

    compiler.ts file cleaning

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,10 +1,10 @@
-import frontend = require('./frontend/frontend');
-import NodeType = require('./frontend/NodeType');
-var fs = require('fs');
+import frontend = require("./frontend/frontend");
+import NodeType = require("./frontend/NodeType");
+var fs = require("fs");
 
 var filename : string = process.argv[2];
-var errorFlag : string = 'error';
-var verbose : string  = 'yes';
+var errorFlag : string = "error";
+var verbose : string  = "yes";
 
 export function compileStr(programStr) {
 	var ast = frontend.parse(programStr);
@@ -12,22 +12,21 @@ export function compileStr(programStr) {
 }
 
 export function compile(filename) {
-  fs.readFile(filename, 'utf8', function (err, programStr) {
+  fs.readFile(filename, "utf8", function (err, programStr) {
     if (err) { throw err; }
     compileStr(programStr);
   });
 }
 
-process.on('uncaughtException', function (err) {
-  if (!err.code) { 
+process.on("uncaughtException", function (err) {
+  if (!err.code) {
     console.log(filename);
-    console.log('Unknown exception occured.');
+    console.log("Unknown exception occured.");
     throw err;
-    process.exit(1);
   }
 
   if (verbose) {
-      console.log(err.name + ': ' + err.message);
+      console.log(err.name + ": " + err.message);
   }
 
   process.exit(err.code);
