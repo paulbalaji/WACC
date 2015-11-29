ccd6ae4ddd6a54eaf33150706b4ae30de27d5308
Paul Balaji <pmb14@ic.ac.uk>
2015-11-29 16:39:06 +0000

    [PB] implemented call node, waiting on funcnode to be completed to do further testing
    Signed-off-by: Paul Balaji <pmb14@ic.ac.uk>

--- src/backend/CodeGenerator.ts
+++ src/backend/CodeGenerator.ts
@@ -33,6 +33,8 @@ export class CodeGenerator implements NodeType.Visitor {
     closingInsertions: any[];
     printNodeLogic: any;
 
+    identOffset: any;
+
     spSubNum: number; // The number of words to subtract from SP at start of main. spSubNum = 1 means SUB sp, sp, #4 will be inserted.
     spSubCurrent: number;
 
@@ -55,6 +57,8 @@ export class CodeGenerator implements NodeType.Visitor {
         this.spSubCurrent = 4;
 
         this.stackMap = {};
+
+        this.identOffset = 0;
 
         this.getNextLabelName = (function() {
             var labelNum = 0;
@@ -593,13 +597,29 @@ export class CodeGenerator implements NodeType.Visitor {
         // leaves result in R0
 
         var functionCall = Instr.Bl('f_' + node.ident.identStr)
-        
-        if (node.argList.length === 0) {
+        var argListSize = node.argList.length;
+
+
+        if (argListSize === 0) {
             return [functionCall];
         }
 
-        
+        var argByteSize = 0;
+        var instrList = [];
 
+        for (var i = argListSize - 1; i >= 0; i--) {
+            var size = CodeGenUtil.getByteSizeFromTypeNode(node.argList[i].type);
+            argByteSize += size;
+            instrList.push(node.argList[i].visit(this),
+                           Instr.Str(Reg.R0, Instr.modify(Instr.Mem(Reg.SP, Instr.Const(-(size))), Instr.mods.bang)));
+            this.identOffset += size;
+        }
+
+        this.identOffset -= argByteSize;
+
+        instrList.push(functionCall, Instr.Add(Reg.SP, Reg.SP, Instr.Const(argByteSize)));
+
+        return instrList;
     }
 
     visitPairLiterNode(node: NodeType.PairLiterNode): any {
@@ -612,7 +632,7 @@ export class CodeGenerator implements NodeType.Visitor {
 
     visitIdentNode(node: NodeType.IdentNode): any {
         var ldrInstruction = (SemanticUtil.isType(node.type, NodeType.BOOL_TYPE, NodeType.CHAR_TYPE)) ? (arg1, arg2) => Instr.modify(Instr.Ldr(arg1, arg2), Instr.mods.sb) : Instr.Ldr;
-        return [ldrInstruction(Reg.R0, Instr.Mem(Reg.SP, Instr.Const(this.currentST.lookUpOffset(node))))]; 
+        return [ldrInstruction(Reg.R0, Instr.Mem(Reg.SP, Instr.Const(this.currentST.lookUpOffset(node) + this.identOffset)))]; 
     }
 
     visitReadNode(node: NodeType.ReadNode): any {
